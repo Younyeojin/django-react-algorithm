749be7c8800eb2a938d50808fde63a425454acfd
Younyeojin <yjyoon2107@gmail.com>
2021-10-28 19:59:00 +0900

    1028

--- backend/admin/user/urls.py
+++ backend/admin/user/urls.py
@@ -4,5 +4,9 @@ from admin.user import views
 
 urlpatterns = {
     url(r'', views.users, name='users'),
-    url(r'^list', views.users)
+    url(r'/login', views.login),
+    url(r'/<slug:id>', views.users),   # delete
+    url(r'', views.users, name='users'),
+    url(r'', views.users, name='users'),
+
 }
--- backend/admin/user/views.py
+++ backend/admin/user/views.py
@@ -10,17 +10,37 @@ from rest_framework.parsers import JSONParser
 from admin.user.models import User
 from admin.user.serializers import UserSerializer
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST', 'PUT'])
 @parser_classes([JSONParser])
 def users(request):
     if request.method == 'GET':    # all
         all_users = User.objects.all()
         serializer = UserSerializer(all_users, many=True)
-        return JsonResponse(data = serializer, safe = False)
+        return JsonResponse(data=serializer, safe = False)
     elif request.method == 'POST':
         ic(request.data)
-        serializer = UserSerializer(data = request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse({'result' : f'Welcome, {serializer.data.get("name")}'}, status=201)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        ic(request.data)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'result': f'Welcome, {serializer.data.get("name")}'}, status=201)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','POST'])
+@parser_classes([JSONParser])
+def users(request, id):
+    pass
+
+
+@api_view(['POST'])
+@parser_classes([JSONParser])
+def login(request):
+    pass
